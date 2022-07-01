b515d373f777c47a46131e89a0a55e58c6425131
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 22:27:50 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR__Week14_2
+{
+    class Program
+    {
+        static void caculate(int s)
+        {
+            Console.WriteLine($"10!(階乘)={s}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR__Week14_2");
+            int s = 1*2*3*4*5*6*7*8*9*10;
+            caculate(s);
+            Console.ReadLine();
+        }
+    }
+}
